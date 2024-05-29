d2a12f0c365c5bc6f8e7f3c8ecab8d95ab887d9d
TecHeReTiC3141 <talex73@mail.ru>
2024-05-29 16:28:17 +0300

    Navbar has been created

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,7 +1,55 @@
 import "./navbar.css";
+import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
+import logo from "../../assets/logo.svg";
+import { useState } from "react";
+
+function LinksMenu() {
+    return (
+        <>
+            <p><a href="#home">Home</a></p>
+            <p><a href="#wgpt3">What is GPT3?</a></p>
+            <p><a href="#possibility">Open AI</a></p>
+            <p><a href="#features">Case Studies</a></p>
+            <p><a href="#blog">Library</a></p>
+        </>
+    )
+}
+
 
 export default function Navbar() {
+
+    const [ toggleMenu, setToggleMenu ] = useState<boolean>(false);
+
     return (
-        <>Navbar</>
+        <div className="gpt3__navbar">
+            <div className="gpt3__navbar-links">
+                <div className="gpt3__navbar-links_logo">
+                    <img src={logo} alt="Logo"/>
+                </div>
+                <div className="gpt3__navbar-links_container">
+                    <LinksMenu />
+                </div>
+            </div>
+            <div className="gpt3__navbar-sign">
+                <p>Sign in</p>
+                <button type="button">Sign up</button>
+            </div>
+            <div className="gpt3__navbar-menu">
+                {toggleMenu ?
+                    <RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(false)}/> :
+                    <RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(true)}/>}
+                {toggleMenu && (
+                    <div className="gpt3__navbar-menu_container scale-up-center">
+                        <div className="gpt3__navbar-menu_container-links">
+                            <LinksMenu/>
+                            <div className="gpt3__navbar-menu_container-links-sign">
+                                <p>Sign in</p>
+                                <button type="button">Sign up</button>
+                            </div>
+                        </div>
+                    </div>
+                )}
+            </div>
+        </div>
     );
 }
--- src/containers/header/Header.tsx
+++ src/containers/header/Header.tsx
@@ -1,7 +1,25 @@
 import "./header.css";
+import people from "../../assets/people.png";
 
 export default function Header() {
     return (
-        <>Header</>
+        <div className="gpt3_header section__padding" id="home">
+            <div className="gpt3__header-content">
+                <h1 className="gradient__text">
+                    Let’s Build Something amazing with GPT-3 OpenAI
+                </h1>
+                <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing.
+                    Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow
+                    asked of.</p>
+                <div className="gpt3__header-content_input">
+                    <input type="email" placeholder="Your email address"/>
+                    <button type="button">Get Started</button>
+                </div>
+                <div className="gpt3__header-content__people">
+                    <img src={people} alt="Subscribers"/>
+                    <p>1,600 people requested access a visit in last 24 hours</p>
+                </div>
+            </div>
+        </div>
     );
 }
