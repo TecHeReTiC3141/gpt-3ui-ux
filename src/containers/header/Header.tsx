59465ec80cbd7caac6bf1c1943757beb6c46754c
TecHeReTiC3141 <talex73@mail.ru>
2024-05-29 17:24:12 +0300

    Header has been created, but it's responsiveness will be improved later

--- src/containers/header/Header.tsx
+++ src/containers/header/Header.tsx
@@ -1,9 +1,10 @@
 import "./header.css";
 import people from "../../assets/people.png";
+import ai from "../../assets/ai.png";
 
 export default function Header() {
     return (
-        <div className="gpt3_header section__padding" id="home">
+        <div className="gpt3__header section__padding" id="home">
             <div className="gpt3__header-content">
                 <h1 className="gradient__text">
                     Let’s Build Something amazing with GPT-3 OpenAI
@@ -11,7 +12,7 @@ export default function Header() {
                 <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing.
                     Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow
                     asked of.</p>
-                <div className="gpt3__header-content_input">
+                <div className="gpt3__header-content__input">
                     <input type="email" placeholder="Your email address"/>
                     <button type="button">Get Started</button>
                 </div>
@@ -19,6 +20,9 @@ export default function Header() {
                     <img src={people} alt="Subscribers"/>
                     <p>1,600 people requested access a visit in last 24 hours</p>
                 </div>
+            </div>
+            <div className="gpt3__header-image">
+                <img src={ai} alt="AI"/>
             </div>
         </div>
     );
