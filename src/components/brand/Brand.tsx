bdff23772a4171d1c83d95b30d01fce6ebe27b4d
TecHeReTiC3141 <talex73@mail.ru>
2024-05-29 17:31:33 +0300

    Brands section has been created

--- src/components/brand/Brand.tsx
+++ src/components/brand/Brand.tsx
@@ -1,7 +1,14 @@
 import "./brand.css";
+import { atlassian, dropbox, google, shopify, slack } from "./imports.ts"
 
 export default function Brand() {
     return (
-        <>Brand</>
+        <div className="gpt3__brand section__padding">
+            <div><img src={google} alt="google"/></div>
+            <div><img src={atlassian} alt="atlassian"/></div>
+            <div><img src={slack} alt="slack"/></div>
+            <div><img src={dropbox} alt="dropbox"/></div>
+            <div><img src={shopify} alt="shopify"/></div>
+        </div>
     );
 }
